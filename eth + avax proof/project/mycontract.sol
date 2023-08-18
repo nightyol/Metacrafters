f33a6fd0042914ffaa6279aab4e6ef117589e03b
tanay rai <96621818+nightyol@users.noreply.github.com>
2023-08-18 12:57:27 +0530

    Update mycontract.sol

--- eth + avax proof/project/mycontract.sol
+++ eth + avax proof/project/mycontract.sol
@@ -1,1 +1,26 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function withdraw(uint amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        
+        uint balance = address(this).balance;
+        require(balance >= amount, "Insufficient contract balance");
+        
+        (bool success, ) = msg.sender.call{value: amount}("");
+        assert(success);
+        
+        revert("Withdrawal failed");
+    }
+
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
 
