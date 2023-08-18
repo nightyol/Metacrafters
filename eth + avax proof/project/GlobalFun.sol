e3e689cfc103cbd1acd6a08c29246d1460e42390
tanay rai <96621818+nightyol@users.noreply.github.com>
2023-08-18 13:00:49 +0530

    Update GlobalFun.sol

--- eth + avax proof/project/GlobalFun.sol
+++ eth + avax proof/project/GlobalFun.sol
@@ -1,1 +1,26 @@
 
+pragma solidity ^0.8.0;
+
+contract MessageContract {
+    string private message;
+
+    event MessageUpdated(string newMessage);
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function updateMessage(string memory newMessage) public {
+        require(validateMessage(newMessage), "Invalid message");
+        message = newMessage;
+        emit MessageUpdated(newMessage);
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function validateMessage(string memory _message) internal pure returns (bool) {
+        return bytes(_message).length > 5;
+    }
+}
